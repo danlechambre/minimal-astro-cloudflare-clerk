0135b04a033f5d8c93db56283529e0777e065a83
Dan Chambers <danlechambre@icloud.com>
2024-10-25 22:16:55 +0100

    Add Clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import clerk from "@clerk/astro";
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [clerk()],
   output: 'hybrid',
   adapter: cloudflare()
 });
